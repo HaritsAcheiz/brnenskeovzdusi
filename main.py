c987f693bac20bdca1203b09a7f0600d5efebcf0
HaritsAcheiz <harits.muhammad.only@gmail.com>
2023-01-28 14:49:05 +0700

    parser built, build to_csv function

--- main.py
+++ main.py
@@ -13,18 +13,19 @@ urls = ['https://www.brnenskeovzdusi.cz/brno-detska-nemocnice/',
         'https://www.brnenskeovzdusi.cz/brno-uvoz/',
         'https://www.brnenskeovzdusi.cz/brno-turany/']
 
-@dataclass
-class AirQuality:
-    station: str
-    datetime: str
-    SO2_1h: float
-    SO2_24h: float
-    NO2_1h: float
-    O3_1h: float
-    O3_8h: float
-    PM10_24h: float
-    PM25_1h: float
-    CO_8h: float
+# @dataclass
+# class AirQuality:
+#     station: str
+#     datetime: str
+#     SO2_1h: float
+#     SO2_24h: float
+#     NO2_1h: float
+#     O3_1h: float
+#     O3_8h: float
+#     PM10_1h: float
+#     PM10_24h: float
+#     PM25_1h: float
+#     CO_8h: float
 
 def make_request(url):
     with httpx.Client() as client:
@@ -32,21 +33,31 @@ def make_request(url):
         html = HTMLParser(response.text)
     return html
 
+def checker(x):
+    if x != None:
+        result = x.text()
+    else:
+        result = None
+    return result
+
 def parse_air_quality(html):
     station = html.css_first('div.box-detail__heading__inner > h1:nth-child(2) > strong:nth-child(1)').text()
     items = html.css('div.respons-table:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr')
+    headers = []
+    datas = []
     for index, item in enumerate(items):
         if index == 0:
-            continue
+           header_objects = item.css('th')
+           for header in header_objects:
+               headers.append(header.text().replace('\t','').replace('\n',''))
         else:
-            new_item = AirQuality(
-                datetime = item.css_first('.text-left.pl40').text(),
-                NO2_1h = item.css_first('.z2').text(),
-
-            )
-
-
+            new_item = dict()
+            for i,header in enumerate(headers,1):
+                new_item[f'{header}'] = item.css_first(f'td:nth-child({i})').text()
+                datas.append(new_item)
+    return datas
 
 if __name__ == '__main__':
     html = make_request(urls[0])
-    parse_air_quality(html)
+    result = parse_air_quality(html)
+    print(result)
